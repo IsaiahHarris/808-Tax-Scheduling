b171ecb58758903bd61853ba94020ae9a2ec1915
Isaiah Harris <harrisfambam@gmail.com>
2019-01-20 17:49:13 -1000

    add knex file

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+const path = require('path');
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'kanban_db',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: path.join(__dirname, 'server', 'db', 'migrations'),
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
