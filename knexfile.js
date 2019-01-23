e3e8dfbbdcc37fe6ce13ca71f0334e97674e46ec
Isaiah Harris <harrisfambam@gmail.com>
2019-01-22 21:06:40 -1000

    setup db table migrations

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'kanban_db',
+      database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASS
     },
