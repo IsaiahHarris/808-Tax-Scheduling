ae313631b22f7acafbe8271d6fec98be384891f2
Isaiah Harris <harrisfambam@gmail.com>
2019-01-22 21:10:43 -1000

    create migrations

---
+++ server/db/migrations/20190122204809_create_appointments_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('appointments', table => {
+    table.increments();
+    table.string('name');
+    table.string('phone_number');
+    table.string('time');
+    table.string('date');
+    table.string('person');
+    table.string('note');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('appointments');
+};
